345fe9fd01289d4ceacce69ccc928903f890cc26
Hugo <42470993+hugobloem@users.noreply.github.com>
2023-03-21 19:40:05 +0000

    settings/secrets template

---
+++ main/secrets_template.h
@@ -0,0 +1,12 @@
+#define HASS_URL "homeassistant.local"
+#define HASS_PORT 8123
+#define HASS_TOKEN "yourtoken"
+#define SR_LANG "en"
+#define NLU_MODE 1 // 0 = hass, 1 = rhasspy
+#define MQTT_URL "mqtt://username:password@homeassistant.local"
+#define MQTT_SITE_ID "esp32" // site id for rhasspy
+#define CONFIG_TZ "GMT0BST,M3.5.0/1,M10.5.0" // Timezone
+
+
+#define NLU_HASS 0
+#define NLU_RHASSPY 1
