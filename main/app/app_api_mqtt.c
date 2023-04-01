3d51383e20c3ef51a99e211df6c50d38bab878c3
Christian-Nils Boda <christian-nils.boda@autoliv.com>
2023-04-01 09:11:06 +0200

    feat: separate MQTT connection information in secrets.h

--- main/app/app_api_mqtt.c
+++ main/app/app_api_mqtt.c
@@ -21,135 +21,142 @@ static bool mqtt_connected = false;
 
 static void log_error_if_nonzero(const char *message, int error_code)
 {
-    if (error_code != 0) {
-        ESP_LOGE(TAG, "Last error %s: 0x%x", message, error_code);
-    }
+  if (error_code != 0)
+  {
+    ESP_LOGE(TAG, "Last error %s: 0x%x", message, error_code);
+  }
 }
-
 
 void data_handler(char *topic_, char *data_, int topic_len, int data_len)
 {
-    // Limit topic and data to 64 and 128 chars max
-    char topic[512];
-    char data[2048];
-    strncpy(topic, topic_, topic_len);
-    strncpy(data, data_, data_len);
-    topic[topic_len] = '\0';
-    data[data_len] = '\0';
+  // Limit topic and data to 64 and 128 chars max
+  char topic[512];
+  char data[2048];
+  strncpy(topic, topic_, topic_len);
+  strncpy(data, data_, data_len);
+  topic[topic_len] = '\0';
+  data[data_len] = '\0';
 
-    // TODO: change to strtok
-    // topic starts with "hermes/"
-    if (strncmp(topic, "hermes/", 7) == 0) {
-        // Handle hermes messages
-        ESP_LOGI(TAG, "hermes message: %s", data);
-        // app_hass_handle_hermes(topic, data);
-    } else if (strncmp(topic, "esp-ha/", 7) == 0) {
-        // Handle esp-ha messages
-        ESP_LOGI(TAG, "esp-ha message: %s", data);
+  // TODO: change to strtok
+  // topic starts with "hermes/"
+  if (strncmp(topic, "hermes/", 7) == 0)
+  {
+    // Handle hermes messages
+    ESP_LOGI(TAG, "hermes message: %s", data);
+    // app_hass_handle_hermes(topic, data);
+  }
+  else if (strncmp(topic, "esp-ha/", 7) == 0)
+  {
+    // Handle esp-ha messages
+    ESP_LOGI(TAG, "esp-ha message: %s", data);
 
-        if (strncmp(topic, "esp-ha/config/add_cmd", 21) == 0) {
-            // Handle config messages
-            ESP_LOGI(TAG, "adding command");
-            app_hass_add_cmd(data);
-        } else if (strncmp(topic, "esp-ha/config/rm_all", 24) == 0) {
-            // Handle config messages
-            ESP_LOGI(TAG, "removing all commands");
-            app_hass_rm_all_cmd(data);
-        }
+    if (strncmp(topic, "esp-ha/config/add_cmd", 21) == 0)
+    {
+      // Handle config messages
+      ESP_LOGI(TAG, "adding command");
+      app_hass_add_cmd(data);
     }
+    else if (strncmp(topic, "esp-ha/config/rm_all", 24) == 0)
+    {
+      // Handle config messages
+      ESP_LOGI(TAG, "removing all commands");
+      app_hass_rm_all_cmd(data);
+    }
+  }
 }
-
 
 static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
 {
-    ESP_LOGD(TAG, "Event dispatched from event loop base=%s, event_id=%d", base, event_id);
-    esp_mqtt_event_handle_t event = event_data;
-    esp_mqtt_client_handle_t client = event->client;
-    int msg_id;
-    switch ((esp_mqtt_event_id_t)event_id) {
-    case MQTT_EVENT_CONNECTED:
-        ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
-        ui_net_config_update_cb(UI_NET_EVT_CLOUD_CONNECTED, NULL);
-        // Set connected flag
-        mqtt_connected = true;
-        break;
-    case MQTT_EVENT_DISCONNECTED:
-        ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
-        ui_net_config_update_cb(UI_NET_EVT_WIFI_CONNECTED, NULL);
-        // Set connected flag
-        mqtt_connected = false;
-        break;
+  ESP_LOGD(TAG, "Event dispatched from event loop base=%s, event_id=%d", base, event_id);
+  esp_mqtt_event_handle_t event = event_data;
+  esp_mqtt_client_handle_t client = event->client;
+  int msg_id;
+  switch ((esp_mqtt_event_id_t)event_id)
+  {
+  case MQTT_EVENT_CONNECTED:
+    ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
+    ui_net_config_update_cb(UI_NET_EVT_CLOUD_CONNECTED, NULL);
+    // Set connected flag
+    mqtt_connected = true;
+    break;
+  case MQTT_EVENT_DISCONNECTED:
+    ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
+    ui_net_config_update_cb(UI_NET_EVT_WIFI_CONNECTED, NULL);
+    // Set connected flag
+    mqtt_connected = false;
+    break;
 
-    case MQTT_EVENT_SUBSCRIBED:
-        ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
-        break;
-    case MQTT_EVENT_UNSUBSCRIBED:
-        ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
-        break;
-    case MQTT_EVENT_PUBLISHED:
-        ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
-        break;
-    case MQTT_EVENT_DATA:
-        ESP_LOGI(TAG, "MQTT_EVENT_DATA");
-        // handle data
-        data_handler(event->topic, event->data, event->topic_len, event->data_len);
-        break;
-    case MQTT_EVENT_ERROR:
-        ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
-        if (event->error_handle->error_type == MQTT_ERROR_TYPE_TCP_TRANSPORT) {
-            log_error_if_nonzero("reported from esp-tls", event->error_handle->esp_tls_last_esp_err);
-            log_error_if_nonzero("reported from tls stack", event->error_handle->esp_tls_stack_err);
-            log_error_if_nonzero("captured as transport's socket errno",  event->error_handle->esp_transport_sock_errno);
-            ESP_LOGI(TAG, "Last errno string (%s)", strerror(event->error_handle->esp_transport_sock_errno));
-
-        }
-        break;
-    case MQTT_EVENT_BEFORE_CONNECT:
-        ESP_LOGI(TAG, "MQTT_EVENT_BEFORE_CONNECT");
-        break;
-    default:
-        ESP_LOGI(TAG, "Other event id:%d", event->event_id);
-        break;
+  case MQTT_EVENT_SUBSCRIBED:
+    ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
+    break;
+  case MQTT_EVENT_UNSUBSCRIBED:
+    ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
+    break;
+  case MQTT_EVENT_PUBLISHED:
+    ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
+    break;
+  case MQTT_EVENT_DATA:
+    ESP_LOGI(TAG, "MQTT_EVENT_DATA");
+    // handle data
+    data_handler(event->topic, event->data, event->topic_len, event->data_len);
+    break;
+  case MQTT_EVENT_ERROR:
+    ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
+    if (event->error_handle->error_type == MQTT_ERROR_TYPE_TCP_TRANSPORT)
+    {
+      log_error_if_nonzero("reported from esp-tls", event->error_handle->esp_tls_last_esp_err);
+      log_error_if_nonzero("reported from tls stack", event->error_handle->esp_tls_stack_err);
+      log_error_if_nonzero("captured as transport's socket errno", event->error_handle->esp_transport_sock_errno);
+      ESP_LOGI(TAG, "Last errno string (%s)", strerror(event->error_handle->esp_transport_sock_errno));
     }
+    break;
+  case MQTT_EVENT_BEFORE_CONNECT:
+    ESP_LOGI(TAG, "MQTT_EVENT_BEFORE_CONNECT");
+    break;
+  default:
+    ESP_LOGI(TAG, "Other event id:%d", event->event_id);
+    break;
+  }
 }
-
 
 // async connect to mqtt server
 esp_err_t mqtt_connect(void)
 {
-    esp_mqtt_client_config_t mqtt_cfg = {
-        .broker.address.uri = MQTT_URL,
-    };
+  esp_mqtt_client_config_t mqtt_cfg = {
+      .broker.address.uri = MQTT_URL,
+      .credentials.username = MQTT_USERNAME,
+      .credentials.authentication.password = MQTT_PASSWORD,
+  };
 
-    client = esp_mqtt_client_init(&mqtt_cfg);
-    /* The last argument may be used to pass data to the event handler, in this example mqtt_event_handler */
-    esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
-    esp_mqtt_client_start(client);
+  client = esp_mqtt_client_init(&mqtt_cfg);
+  /* The last argument may be used to pass data to the event handler, in this example mqtt_event_handler */
+  esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
+  esp_mqtt_client_start(client);
 
-    // Wait for connection
-    while (!mqtt_connected) {
-        vTaskDelay(100 / portTICK_PERIOD_MS);
-    }
+  // Wait for connection
+  while (!mqtt_connected)
+  {
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+  }
 
-    return ESP_OK;
+  return ESP_OK;
 }
 
 /* start mqtt client */
 void app_api_mqtt_start(void)
 {
-    ESP_ERROR_CHECK(mqtt_connect());
+  ESP_ERROR_CHECK(mqtt_connect());
 
-    // Subscribe to hermes and configuration topics
-    esp_mqtt_client_subscribe(client, "hermes/#", 0);
-    esp_mqtt_client_subscribe(client, "esp-ha/#", 0);
-
+  // Subscribe to hermes and configuration topics
+  esp_mqtt_client_subscribe(client, "hermes/#", 0);
+  esp_mqtt_client_subscribe(client, "esp-ha/#", 0);
 }
 
 /* send commands to mqtt */
 void app_api_mqtt_send_cmd(char *topic, char *cmd)
 {
-    char *payload = malloc(strlen(cmd) + 100);
-    sprintf(payload, "{\"input\": \"%s\", \"siteId\": \"%s\"}", cmd, MQTT_SITE_ID);
+  char *payload = malloc(strlen(cmd) + 100);
+  sprintf(payload, "{\"input\": \"%s\", \"siteId\": \"%s\"}", cmd, MQTT_SITE_ID);
 
-    esp_mqtt_client_publish(client, topic, payload, 0, 0, 0);
+  esp_mqtt_client_publish(client, topic, payload, 0, 0, 0);
 }
--- main/secrets_template.h
+++ main/secrets_template.h
@@ -4,10 +4,12 @@
 #define HASS_PORT 8123
 #define HASS_TOKEN "yourtoken"
 #define SR_LANG "en"
-#define NLU_MODE 1 // 0 = hass, 1 = rhasspy
-#define MQTT_URL "mqtt://username:password@homeassistant.local"
-#define MQTT_SITE_ID "esp32"                 // site id for rhasspy
-#define CONFIG_TZ "GMT0BST,M3.5.0/1,M10.5.0" // Timezone
+#define NLU_MODE 1                            // 0 = hass, 1 = rhasspy
+#define MQTT_URL "mqtt://homeassistant.local" // your MQTT server address, if using an IP address, don't forget the port
+#define MQTT_USERNAME "mqttuser"              // your MQTT username
+#define MQTT_PASSWORD "mqttpass"              // your MQTT password
+#define MQTT_SITE_ID "esp32"                  // site id for rhasspy
+#define CONFIG_TZ "GMT0BST,M3.5.0/1,M10.5.0"  // Timezone
 
 #define NLU_HASS 0
 #define NLU_RHASSPY 1
