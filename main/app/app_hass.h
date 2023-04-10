983cdce05d78881e1b24f5e8a73d71d23ab39b67
Hugo <42470993+hugobloem@users.noreply.github.com>
2023-04-10 22:54:53 +0100

    refactor: mqtt topic handling to use strtok

--- main/app/app_hass.h
+++ main/app/app_hass.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <esp_err.h>
+#include "cJSON.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -11,8 +12,8 @@ bool app_hass_is_connected(void);
 void app_hass_send_cmd(char *cmd);
 
 void app_hass_add_cmd(char *cmd, char *phoneme, bool commit);
-void app_hass_add_cmd_from_msg(char *msg);
-void app_hass_rm_all_cmd();
+void app_hass_add_cmd_from_msg(cJSON *root);
+void app_hass_rm_all_cmd(cJSON *root);
 
 #ifdef __cplusplus
 }
